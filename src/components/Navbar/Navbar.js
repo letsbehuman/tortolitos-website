fa58c07f96376751e7bcbb4806d0da8071a56d00
letsbehuman <letsbehumandesign@gmail.com>
2022-10-04 15:50:38 +0200

    WIP responsive navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,42 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.scss';
-// import Isologo from './../../img/logonav.png';
-// import Logo from './../../img/logoNavbar.png';
+import { TiThMenu } from 'react-icons/ti';
 
 const Navbar = () => {
+  const [navbarMenu, setNavbarMenu] = useState(false);
+  const handleMenu = () => {
+    setNavbarMenu(!navbarMenu);
+  };
+
   return (
     <nav className="navbar">
       <a className="navbar-logo" href="/#home">
-        <img alt="logo" className="logo" src="/img/logonav.png" />
-        <img alt="logo" className="isologo" src="img/logonav.png" />
+        <img alt="logo" className="logo" src="/img/logoNavbar.png" />
+        <img alt="logo" className="isologo" src="/img/logonav.png" />
       </a>
       <ul className="nav-links">
         <li>
           <a className="nav-link" href="/#team">
-            <span className="non-essencial">Our</span>team
+            <span className="non-essencial">{'Our'}</span>
+            {'team'}
           </a>
         </li>
         <li>
           <a className="nav-link" href="/#products">
-            <span className="non-essencial">Our</span>stuff
+            <span className="non-essencial">{'Our'}</span>
+            {'stuff'}
           </a>
         </li>
         <li>
           <a className="nav-link" href="/#party-time">
-            Party<span className="non-essencial">time</span>
+            {'Party'}
+            <span className="non-essencial">{'time'}</span>
           </a>
         </li>
         <li>
           <a className="nav-link" href="/#contact">
-            Contact
+            {'Contact'}
           </a>
         </li>
         <li>
           <a className="nav-link" href="/#events">
-            Events
+            {'Events'}
           </a>
         </li>
+        <button className="menu-icon" onClick={() => handleMenu()}>
+          <TiThMenu />
+        </button>
       </ul>
+      <div className="extended-container"></div>
     </nav>
   );
 };
