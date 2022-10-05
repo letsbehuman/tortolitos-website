c1a8a493228159a91720c00af3fc63469f18f5a4
letsbehuman <letsbehumandesign@gmail.com>
2022-10-05 19:46:52 +0200

    fixing UI navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,53 +1,94 @@
 import React, { useState } from 'react';
 import './navbar.scss';
 import { TiThMenu } from 'react-icons/ti';
+import { CgClose } from 'react-icons/cg';
 
 const Navbar = () => {
   const [navbarMenu, setNavbarMenu] = useState(false);
-  const handleMenu = () => {
-    setNavbarMenu(!navbarMenu);
+  const handleMenu = (str) => {
+    if (str === 'open') return setNavbarMenu(true);
+    if (str === 'close') return setNavbarMenu(false);
   };
 
   return (
     <nav className="navbar">
-      <a className="navbar-logo" href="/#home">
-        <img alt="logo" className="logo" src="/img/logoNavbar.png" />
-        <img alt="logo" className="isologo" src="/img/logonav.png" />
-      </a>
-      <ul className="nav-links">
-        <li>
-          <a className="nav-link" href="/#team">
-            <span className="non-essencial">{'Our'}</span>
-            {'team'}
-          </a>
-        </li>
-        <li>
-          <a className="nav-link" href="/#products">
-            <span className="non-essencial">{'Our'}</span>
-            {'stuff'}
-          </a>
-        </li>
-        <li>
-          <a className="nav-link" href="/#party-time">
-            {'Party'}
-            <span className="non-essencial">{'time'}</span>
-          </a>
-        </li>
-        <li>
-          <a className="nav-link" href="/#contact">
-            {'Contact'}
-          </a>
-        </li>
-        <li>
-          <a className="nav-link" href="/#events">
-            {'Events'}
-          </a>
-        </li>
-        <button className="menu-icon" onClick={() => handleMenu()}>
+      <div className="navbar-desktop">
+        <a className="navbar-logo" href="/#home">
+          <img alt="logo" className="isologo" src="/img/logonav.png" />
+          <img alt="logo" className="logo" src="/img/logoNavbar.png" />
+        </a>
+        <ul className="nav-links">
+          <li>
+            <a className="nav-link" href="/#team">
+              <span className="non-essencial">{'Our'}</span>
+              {'team'}
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="/#products">
+              <span className="non-essencial">{'Our'}</span>
+              {'stuff'}
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="/#party-time">
+              {'Party'}
+              <span className="non-essencial">{'time'}</span>
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="/#contact">
+              {'Contact'}
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="/#events">
+              {'Events'}
+            </a>
+          </li>
+        </ul>
+        <button
+          className={navbarMenu === false ? 'menu-icon' : 'hidden'}
+          onClick={() => handleMenu('open')}
+        >
           <TiThMenu />
         </button>
-      </ul>
-      <div className="extended-container"></div>
+      </div>
+      {navbarMenu && (
+        <div className="navbar-mobile" onClick={() => handleMenu('close')}>
+          <div className="drop-menu">
+            <button className="close-icon" onClick={() => handleMenu('close')}>
+              X
+            </button>
+            <ul className="nav-links--mobile">
+              <li>
+                <a href="/#team">
+                  <span className="non-essencial">{'Our'}</span>
+                  {'team'}
+                </a>
+              </li>
+              <li>
+                <a href="/#products">
+                  <span className="non-essencial">{'Our'}</span>
+                  {'stuff'}
+                </a>
+              </li>
+              <li>
+                <a href="/#party-time">
+                  {'Party'}
+                  <span className="non-essencial">{'time'}</span>
+                </a>
+              </li>
+              <li>
+                <a href="/#contact">{'Contact'}</a>
+              </li>
+              <li>
+                <a href="/#events">{'Events'}</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -9,11 +9,62 @@ import PartyTime from './PartyTime/PartyTime';
 import Contact from './Contact/Contact';
 import Events from './Events/Events';
 import Footer from '../components/Footer/Footer';
-
+import { SpeedDial, SpeedDialAction, SpeedDialIcon } from '@mui/material/';
+import { MdEmail } from 'react-icons/md';
+import { BsFacebook } from 'react-icons/bs';
+import { AiFillInstagram } from 'react-icons/ai';
 function App() {
   return (
     <div className="">
       <Navbar />
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{
+          position: 'fixed',
+          bottom: 16,
+          right: 16,
+        }}
+        icon={<SpeedDialIcon />}
+      >
+        <SpeedDialAction
+          icon={
+            <a
+              className="speedDialAction"
+              href="https://www.instagram.com/tortolitos_berlin/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillInstagram />
+            </a>
+          }
+          tooltipTitle="Instagram"
+        />
+        <SpeedDialAction
+          className="speedDialAction"
+          icon={
+            <a
+              className="speedDialAction"
+              href="https://www.instagram.com/tortolitos_berlin/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsFacebook />
+            </a>
+          }
+          tooltipTitle="Facebook"
+        />
+        <SpeedDialAction
+          icon={
+            <a
+              className="speedDialAction"
+              href="mailto:tortolitosberlin@gmail.com"
+            >
+              <MdEmail />
+            </a>
+          }
+          tooltipTitle="Email"
+        />
+      </SpeedDial>
       <Header />
       <Banner />
       <OurTeam />
